04006d7850a1a7dea789d3b965455280daf6f018
Peter Aryanto <peter.aryanto@gmail.com>
2023-02-17 13:48:10 +1100

    Added Identity Server to the services.

--- IDS/Program.cs
+++ IDS/Program.cs
@@ -1,5 +1,19 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+  .AddInMemoryClients(new List<Client>())
+  .AddInMemoryIdentityResources(new List<IdentityResource>())
+  .AddInMemoryApiResources(new List<ApiResource>())
+  .AddInMemoryApiScopes(new List<ApiScope>())
+  .AddTestUsers(new List<TestUser>())
+  .AddDeveloperSigningCredential();
+
 var app = builder.Build();
+
+app.UseIdentityServer();
 
 app.MapGet("/", () => "Hello World!");
 
